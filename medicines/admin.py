4db3f8c213369dcffc22463e54374640a86fc059
mayte.bujalance <maytecasas252@gmail.com>
2025-07-03 15:03:51 +0100

    add Ingredient and Company models; update Medicine model to include ingredients, company, and price; register models in admin

--- medicines/admin.py
+++ medicines/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Medicine
+from .models import Medicine,Ingredient, Company
+
+# Register your models here.
 
 admin.site.register(Medicine)
-# Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(Company)
+# The above code registers the Medicine, Ingredient, and Company models with the Django admin site.
+# This allows these models to be managed through the Django admin interface, enabling CRUD operations on them
--- medicines/models.py
+++ medicines/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 
-# Create your models here.
+# Create the model here
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 
 class Medicine(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
+    ingredients = models.ManyToManyField(Ingredient)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, default=1)  # 1-off default
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
 
     def __str__(self):
         return self.name
