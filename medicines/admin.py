8ef570d0aef9cc1c4854d4d3ca83ce8fa07ddac0
mayte.bujalance <maytecasas252@gmail.com>
2025-07-04 09:51:05 +0100

    refactor: clean up imports and update URL routing for medicines app

--- medicines/admin.py
+++ medicines/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Medicine,Ingredient, Company
+from .models import Medicine, Ingredient, Company
+
 
 # Register your models here.
 
--- medicines/views.py
+++ medicines/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Medicine
+from .models import Medicine, Ingredient
 
 # Create your views here.
 
