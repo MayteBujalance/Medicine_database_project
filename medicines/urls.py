783ee0ef3d3c35e051c04cac559ff7be11be9db8
mayte.bujalance <maytecasas252@gmail.com>
2025-07-04 12:15:32 +0100

    feat: implement search functionality and update views; add index and search templates

--- medicines/urls.py
+++ medicines/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('search/', views.search_medicines, name='search_medicines'),
+    path('', views.index, name='index'),  # this shows all medicines
+    path('search/', views.search, name='search'),  # this lets users search
 ]
--- medicines/views.py
+++ medicines/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from .models import Medicine, Ingredient
 
-# Create your views here.
-
-
+# View to show all medicines
 def index(request):
     medicines = Medicine.objects.all()
     return render(request, 'medicines/index.html', {'medicines': medicines})
+
+# View to search by ingredient
+def search(request):
+    query = request.GET.get('ingredient')
+    medicines = []
+
+    if query:
+        medicines = Medicine.objects.filter(ingredients__name__icontains=query).distinct()
+
+    return render(request, 'medicines/search.html', {
+        'query': query,
+        'medicines': medicines
+    })
